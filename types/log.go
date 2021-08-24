5167a528dd360b740ed37abab356d1bde023e99f
wanliqun <wanliqunn@163.com>
2021-08-24 14:59:29 +0800

    Add FromBlock/ToBlock field in types.LogFilter (#104)
    Also add unmarshall fromBlock && toBlock to types.LogFilter

--- types/log.go
+++ types/log.go
@@ -20,6 +20,8 @@ import (
 type LogFilter struct {
 	FromEpoch   *Epoch          `json:"fromEpoch,omitempty"`
 	ToEpoch     *Epoch          `json:"toEpoch,omitempty"`
+	FromBlock   *hexutil.Big    `json:"fromBlock,omitempty"`
+	ToBlock     *hexutil.Big    `json:"toBlock,omitempty"`
 	BlockHashes []Hash          `json:"blockHashes,omitempty"`
 	Address     []Address       `json:"address,omitempty"`
 	Topics      [][]Hash        `json:"topics,omitempty"`
@@ -127,6 +129,8 @@ func (l *LogFilter) UnmarshalJSON(data []byte) error {
 	type tmpLogFilter struct {
 		FromEpoch   *Epoch          `json:"fromEpoch,omitempty"`
 		ToEpoch     *Epoch          `json:"toEpoch,omitempty"`
+		FromBlock   *hexutil.Big    `json:"fromBlock,omitempty"`
+		ToBlock     *hexutil.Big    `json:"toBlock,omitempty"`
 		BlockHashes []Hash          `json:"blockHashes,omitempty"`
 		Address     interface{}     `json:"address,omitempty"`
 		Topics      []interface{}   `json:"topics,omitempty"`
@@ -141,6 +145,8 @@ func (l *LogFilter) UnmarshalJSON(data []byte) error {
 	var err error
 	l.FromEpoch = t.FromEpoch
 	l.ToEpoch = t.ToEpoch
+	l.FromBlock = t.FromBlock
+	l.ToBlock = t.ToBlock
 	l.BlockHashes = t.BlockHashes
 	l.Limit = t.Limit
 	if l.Address, err = resolveToAddresses(t.Address); err != nil {
