d14aa1d862b0c7066f66368d83b7089bd47d24cc
dayong <dayong@conflux-chain.org>
2021-11-11 17:52:27 +0800

    remove prefix

--- client_txpool.go
+++ client_txpool.go
@@ -13,39 +13,39 @@ func NewRpcTxpoolClient(core *Client) RpcTxpoolClient {
 	return RpcTxpoolClient{core}
 }
 
-func (c *RpcDebugClient) TxpoolStatus() (val types.TxPoolStatus, err error) {
+func (c *RpcTxpoolClient) Status() (val types.TxPoolStatus, err error) {
 	err = c.core.CallRPC(&val, "txpool_status")
 	return
 }
 
-func (c *RpcDebugClient) TxpoolNextNonce(address types.Address) (val *hexutil.Big, err error) {
+func (c *RpcTxpoolClient) NextNonce(address types.Address) (val *hexutil.Big, err error) {
 	err = c.core.CallRPC(&val, "txpool_nextNonce", address)
 	return
 }
 
-func (c *RpcDebugClient) TxpoolTransactionByAddressAndNonce(address types.Address, nonce *hexutil.Big) (val *types.Transaction, err error) {
+func (c *RpcTxpoolClient) TransactionByAddressAndNonce(address types.Address, nonce *hexutil.Big) (val *types.Transaction, err error) {
 	err = c.core.CallRPC(&val, "txpool_transactionByAddressAndNonce", address, nonce)
 	return
 }
 
-func (c *RpcDebugClient) TxpoolPendingNonceRange(address types.Address) (val types.TxPoolPendingNonceRange, err error) {
+func (c *RpcTxpoolClient) PendingNonceRange(address types.Address) (val types.TxPoolPendingNonceRange, err error) {
 	err = c.core.CallRPC(&val, "txpool_pendingNonceRange", address)
 	return
 }
 
-func (c *RpcDebugClient) TxpoolTxWithPoolInfo(hash types.Hash) (val types.TxWithPoolInfo, err error) {
+func (c *RpcTxpoolClient) TxWithPoolInfo(hash types.Hash) (val types.TxWithPoolInfo, err error) {
 	err = c.core.CallRPC(&val, "txpool_txWithPoolInfo", hash)
 	return
 }
 
 /// Get transaction pending info by account address
-func (c *RpcDebugClient) AccountPendingInfo(address types.Address) (val *types.AccountPendingInfo, err error) {
+func (c *RpcTxpoolClient) AccountPendingInfo(address types.Address) (val *types.AccountPendingInfo, err error) {
 	err = c.core.CallRPC(&val, "txpool_accountPendingInfo", address)
 	return
 }
 
 /// Get transaction pending info by account address
-func (c *RpcDebugClient) AccountPendingTransactions(address types.Address, maybeStartNonce *hexutil.Big, maybeLimit *hexutil.Uint64) (val types.AccountPendingTransactions, err error) {
+func (c *RpcTxpoolClient) AccountPendingTransactions(address types.Address, maybeStartNonce *hexutil.Big, maybeLimit *hexutil.Uint64) (val types.AccountPendingTransactions, err error) {
 	err = c.core.CallRPC(&val, "txpool_accountPendingTransactions", address, maybeStartNonce, maybeLimit)
 	return
 }
