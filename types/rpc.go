0865bbcd604a2985a5dc21810924ef457d297562
dayong <dayong@conflux-chain.org>
2021-12-30 11:49:55 +0800

    update struct status and storageroot

--- types/rpc.go
+++ types/rpc.go
@@ -54,8 +54,8 @@ type SponsorInfo struct {
 // Status represents current blockchain status
 type Status struct {
 	BestHash         Hash           `json:"bestHash"`
-	ChainID          hexutil.Uint   `json:"chainId"`
-	NetworkID        hexutil.Uint   `json:"networkId"`
+	ChainID          hexutil.Uint64 `json:"chainId"`
+	NetworkID        hexutil.Uint64 `json:"networkId"`
 	EpochNumber      hexutil.Uint64 `json:"epochNumber"`
 	BlockNumber      hexutil.Uint64 `json:"blockNumber"`
 	PendingTxNumber  hexutil.Uint64 `json:"pendingTxNumber"`
@@ -65,9 +65,9 @@ type Status struct {
 }
 
 type StorageRoot struct {
-	Delta        Hash `json:"delta"`        //delta: H256,
-	Intermediate Hash `json:"intermediate"` //intermediate: H256,
-	Snapshot     Hash `json:"snapshot"`     //snapshot: H256,
+	Delta        *Hash `json:"delta"`        //delta: H256,
+	Intermediate *Hash `json:"intermediate"` //intermediate: H256,
+	Snapshot     *Hash `json:"snapshot"`     //snapshot: H256,
 }
 
 type CheckBalanceAgainstTransactionResponse struct {
