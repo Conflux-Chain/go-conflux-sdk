4b42a6b06558dd0bbf902454715feb840388a729
dayong <dayong@conflux-chain.org>
2022-01-06 16:06:56 +0800

    Compatible hexutil.Uint64 and uint64 for unmarshal VoteStakeInfo

--- types/vote_stake_info.go
+++ types/vote_stake_info.go
@@ -1,6 +1,11 @@
 package types
 
-import "github.com/ethereum/go-ethereum/common/hexutil"
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
 
 // VoteStakeInfo represents user vote history
 type VoteStakeInfo struct {
@@ -10,4 +15,30 @@ type VoteStakeInfo struct {
 	/// This is the timestamp when the vote right will be invalid, measured in
 	/// the number of past blocks.
 	UnlockBlockNumber uint64 `json:"unlockBlockNumber"`
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (v *VoteStakeInfo) UnmarshalJSON(data []byte) error {
+	tmp := struct {
+		Amount            *hexutil.Big `json:"amount"`
+		UnlockBlockNumber interface{}  `json:"unlockBlockNumber"`
+	}{}
+
+	err := json.Unmarshal(data, &tmp)
+	if err != nil {
+		return err
+	}
+
+	switch tmp.UnlockBlockNumber.(type) {
+	case float64:
+		v.UnlockBlockNumber = uint64(tmp.UnlockBlockNumber.(float64))
+	case string:
+		val, err := strconv.ParseUint(tmp.UnlockBlockNumber.(string), 0, 64)
+		if err != nil {
+			return err
+		}
+		v.UnlockBlockNumber = val
+	}
+	v.Amount = tmp.Amount
+	return nil
 }
---
+++ types/vote_stake_info_test.go
@@ -0,0 +1,25 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestUnmarshal(t *testing.T) {
+
+	cases := []string{
+		`{"amount":"0x64","unlockBlockNumber":200}`,
+		`{"amount":"0x64","unlockBlockNumber":"0x64"}`,
+		fmt.Sprintf(`{"amount":"0x64","unlockBlockNumber":%v}`, ^uint64(0)),
+	}
+
+	for _, c := range cases {
+		v := VoteStakeInfo{}
+		e := json.Unmarshal([]byte(c), &v)
+		if e != nil {
+			t.Fatal(e)
+		}
+		fmt.Println(v)
+	}
+}
